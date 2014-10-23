404a9175dad8d745bebb070391adaad9901a9fb9
james2doyle <james2doyle@gmail.com>
2014-10-23 11:21:12 -0400

    more comments. compress images

--- public/js/script.js
+++ public/js/script.js
@@ -25,22 +25,25 @@ function handleTweet(tweet) {
   // dont serve the tiny profile pics
   var profile = (tweet.user.profile_image_url_https).replace('_normal', '');
   var newElem = document.createElement('div');
+  newElem.className = 'tweet';
   // <img src=\""+profile+"\" height=\"200\" width=\"200\" title=\""+tweet.user.screen_name+"\">
   var template = "<div class=\"tweet-image\" ><a href=\"https://twitter.com/"+tweet.user.screen_name+"\" title=\""+tweet.user.screen_name+"\"></a><a href=\"https://twitter.com/@"+tweet.user.screen_name+"\" class=\"tweet-user\">@"+tweet.user.screen_name+"</a></div><div class=\"tweet-text\"><p>"+tweet.tweet+"</p></div>";
-  newElem.className = 'tweet';
   // if there are image attachments
   if (typeof(tweet.entities.media) === 'object') {
     Array.prototype.slice.call(tweet.entities.media, 0).forEach(function(item){
-      template += "<div class=\"tweet-media\"><img src=\""+item.media_url_https+"\" alt=\"\"></div>";
+      template += "<div class=\"tweet-media\"><img src=\""+item.media_url_https+"\" alt=\""+tweet.user.screen_name+" media attachment\"></div>";
     });
   }
   newElem.innerHTML = template;
   var clearfix = document.createElement('div');
+  // thow in a clearfix
   clearfix.className = "clearfix";
   content.insertBefore(clearfix, content.firstChild);
+  // insert the new tweet at the top of the container
   content.insertBefore(newElem, content.firstChild);
   // fadein the image when the profile pic has loaded
   loadedImage(profile, function(temp) {
+    // inject the image into the tweet
     newElem.querySelector('.tweet-image a').appendChild(temp);
     newElem.classList.add('fadeIn');
   });
